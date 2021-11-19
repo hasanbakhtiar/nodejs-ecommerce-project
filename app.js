9a03589e6fafb347ec2103d8ae50d24988743191
Hasan Bakhtiar <52384595+HasanBakhtiar@users.noreply.github.com>
2021-11-20 01:53:18 +0400

    NodeJS Ecommerce v0.1

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+// Connect to db
+mongoose.connect(config.database);
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, 'connection error:'));
+db.once('open',()=>{
+    console.log('Connected to MongoDB')
+});
+
+// Init app
+const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Express Session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+  }))
+
+app.get('/',(req,res)=>{
+res.render('index',{
+    title: "Home"
+});
+})
+
+// Start the server
+const PORT = 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server start on port: ${PORT}`);
+})
